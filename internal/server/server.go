c4da588426008368773f9333c34cabdfbf4da758
Mirko-A <adzicmirko97@gmail.com>
2024-07-16 08:18:07 +0200

    Remove health check route

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,36 +10,26 @@ type PredictionRequest struct {
 	PixelArray []int `json:"pixelArray"`
 }
 
-func healthCheckHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "doodle-guessr server is running\n")
-}
-
-func predictHandler(w http.ResponseWriter, r *http.Request) {
-	// Initialize the struct to hold the request body.
+func predictHandler(rw http.ResponseWriter, request *http.Request) {
 	var req PredictionRequest
 
-	// Parse the JSON body into the struct.
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := json.NewDecoder(request.Body).Decode(&req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Access the pixelArray from the struct.
 	pixelArray := req.PixelArray
 
-	// Your logic here, for example, logging the pixelArray or processing it.
 	fmt.Println(pixelArray)
 
-	// Respond to the client.
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"status": "success"})
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(map[string]string{"status": "success"})
 }
 
 func Init() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/health-check", healthCheckHandler)
 	http.HandleFunc("/predict", predictHandler)
 }
 
