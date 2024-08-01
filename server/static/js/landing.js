5ea4d0abfe91d6745a7d1cc1c8438f3610f2f1a3
Mirko-A <adzicmirko97@gmail.com>
2024-08-01 11:29:24 +0200

    Revert "Add scores endpoint test code"
    This reverts commit 9800d72e91a1693524da40099515c366a690a6fe.

--- server/static/js/landing.js
+++ server/static/js/landing.js
@@ -1,30 +1,5 @@
 const startBtn = document.getElementById("startBtn");
-const testSetBtn = document.getElementById("testSetBtn");
-const testReadBtn = document.getElementById("testReadBtn");
 
 startBtn.onclick = () => {
     window.location.href = "/draw.html";
-};
-
-testSetBtn.onclick = async () => {
-    const response = await fetch("http://localhost:3000/api/scores", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            name: "prki",
-            points: 35,
-        }),
-    });
-
-    console.log(response);
-};
-
-testReadBtn.onclick = async () => {
-    const response = await fetch("http://localhost:3000/api/scores", {
-        method: "GET",
-    });
-
-    console.log(response);
 };
